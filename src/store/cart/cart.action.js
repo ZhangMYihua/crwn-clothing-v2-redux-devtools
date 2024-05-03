cb094b04321b52a0e4a66cf5aa695bb0a5f1745e
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2024-05-03 22:43:10 +0100

    cart reducer using rtk

--- src/store/cart/cart.action.js
+++ src/store/cart/cart.action.js
@@ -1,5 +1,5 @@
-import { createAction } from '../../utils/reducer/reducer.utils';
-import { CART_ACTION_TYPES } from './cart.types';
+import { cartActions } from "./cart.reducer";
+
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -41,18 +41,15 @@ const clearCartItem = (cartItems, cartItemToClear) =>
 
 export const addItemToCart = (cartItems, productToAdd) => {
   const newCartItems = addCartItem(cartItems, productToAdd);
-  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+  return cartActions.setCartItems(newCartItems)
 };
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const newCartItems = removeCartItem(cartItems, cartItemToRemove);
-  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+  return cartActions.setCartItems(newCartItems)
 };
 
 export const clearItemFromCart = (cartItems, cartItemToClear) => {
   const newCartItems = clearCartItem(cartItems, cartItemToClear);
-  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+  return cartActions.setCartItems(newCartItems)
 };
-
-export const setIsCartOpen = (boolean) =>
-  createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean);
